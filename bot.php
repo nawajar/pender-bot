c4016bd7f93d7472ffcc052b481544c36e824506
Nawa Jarutamrong <nawa.j@kasikornbank.com>
2017-08-21 17:51:10 +0700

    test

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $event
+				'text' => json_encode($event)
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
