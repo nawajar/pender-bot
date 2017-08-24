fda45c46b9bddf7699be4b2606a23d149062aa0d
Nawa Jarutamrong <nawa.j@kasikornbank.com>
2017-08-25 00:39:01 +0700

    abc

--- bot.php
+++ bot.php
@@ -1,28 +1,7 @@
 <?php
 
 
-function calc_string( $mathString )
-{
-        $cf_DoCalc = create_function("", "return (" . $mathString . ");" );   
-        return $cf_DoCalc();
-};
-
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-};
-
-
-
-
-
 $access_token = 'ae6P1wQm9pDtBXz1TQNnAqWJSUHvIiUl0GWPJNvLK8MQxYuPIaqaP+Kea9H6QcnyVCyw2iJILvy00zXyV/B9nIB+NAeP9P9da7HZxbk0atcm2tYeuXngrKaMBMWwMy3msa5PEluN2bGu0JI7enTELwdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -30,8 +9,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
-	foreach ($events['events'] as $event) 
-	{
+	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
@@ -39,38 +17,25 @@ if (!is_null($events['events'])) {
 			$user = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			if($user == 'Ua0afffd8ddfae4569dff6ab0abb17ee4'){
 			$msg = "";	
-			$fnc = explode(">", $text);
-			if($user == 'Ua0afffd8ddfae4569dff6ab0abb17ee4')
-			{
-			
-
-						if($fnc[0] == 'bot:type')
-						{
-							$msg = $fnc[1];
-							sendMsg($msg , $replyToken);
-		
-						}else if ($fnc[0] == 'bot:time'){
-							$today = date("D M j G:i:s T Y");	
-					     	$msg = String($today);	
-					     	sendMsg($msg , $replyToken);
-						}
-
-		// Build message to reply back
- 		 $messages = [
+			$fnc = explode(" ", $text);
+				if($fnc[0] == 'bot:type'){
+					$msg = $fnc[1];
+					
+					// Build message to reply back
+			$messages = [
 				'type' => 'text',
 				'text' => $msg
 			];
-
-			
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -79,55 +44,20 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo $result . "\r\n";		
+			echo $result . "\r\n";
+				
+			}else if ($fnc[0] == "bot:grean"){
+					
+		     	
+					
+			}		
+			
+			
+				
+			}	
 				
 			
-			}else{
-					if($fnc[0] == 'bot:cal')
-						{
-							$msg = calc_string($fnc[1]);
-							sendMsg($msg , $replyToken);
-		
-						}else if($fnc[0] == 'bot:randomstr'){
-							$msg = generateRandomString($fnc[1]);
-							sendMsg($msg , $replyToken);
-						}else if($fnc[0] == 'bot:randomint'){
-							$item = explode(",", $fnc[1]);
-							$a = mt_rand($item[0],$item[1]);
-							sendMsg($a , $replyToken);
-						}
-				// Build message to reply back
- 		 $messages = [
-				'type' => 'text',
-				'text' => $msg
-			];
-
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			echo $result . "\r\n";				
-
-
-			}
 		}
 	}
-
 }
-
- 
-
 echo "OK";
